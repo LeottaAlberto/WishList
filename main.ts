79d4597d30ec74b24e2c048ccc6f70921268fa8e
LeottaAlberto <leottaalberto43@gmail.com>
2024-03-13 16:17:56 +0100

    init
    Init project

---
+++ main.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+
+app.use(express.json())
+
+
+// async per le le funzioni asincrone e per fare le richieste al DB
+app.post('/', async (req, res) => {
+    const body=req.body
+    res.send("");
+
+});
+
+
+app.listen(PORT, "0.0.0.0", ()=>{
+    console.log('Server in ascolto sulla porta: ' + PORT);
+});
---
+++ test/testdb.ts
@@ -0,0 +1,21 @@
+import { Prisma } from "../src/db";
+
+async function test() {
+    await Prisma.$connect();
+    const ciao = await Prisma.user.create({
+        data: {
+            username: 'Pasticcio',
+            password: '123prova',
+            email: 'pasticcio@ciao.it',
+        }
+    });
+    console.log(ciao);
+    
+}
+
+test().catch((err) => {
+    console.error('Error: ', err);
+    process.exit(1);
+}).finally(async ()=>{
+    await Prisma.$disconnect();
+});
