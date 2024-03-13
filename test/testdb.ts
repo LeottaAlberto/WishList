e6c1548d6657c54907b75f1e8fe1f3021b084b2a
LeottaAlberto <leottaalberto43@gmail.com>
2024-03-13 17:37:46 +0100

    create function delete
    doesn't work :)

--- test/testdb.ts
+++ test/testdb.ts
@@ -21,13 +21,55 @@ async function test() {
     console.log(check);
 }
 
+/**
+ * 
+ * @param user 
+ * @param category 
+ * @param priority 
+ * @param description 
+ */
 async function createGift(user: string, category: string, priority: string, description: string) {
-    // const newGift = await Prisma..create({
-
-    // });
+    const newGift = await Prisma.gift.create({
+        data: {
+            user: user,
+            category: category,
+            priority: priority,
+            description: description,
+        }
+    });
+    console.log(newGift);
+    checkGift(newGift.id);
 }
 
-test().catch((err) => {
+async function checkGift(id: string) {
+    const gift = await Prisma.gift.findFirst({
+        where: {
+            id: id
+        }
+    });
+    if(gift !== null) {
+        console.log("regalo esistente");
+        deleteGift(gift.id);
+    }
+    else console.log("regalo non esistente");
+    
+}
+
+async function deleteGift(id: string) {
+    const deleteGift = await Prisma.gift.delete({
+        where: {
+            id: id,
+        },
+    });
+}
+
+// createGift('65f1bacc3037064e96f5bbcf', '1', '100', 'regalo bellissimo che vorrei avere sempre con me').catch((err) => {
+//     console.error('Error: ', err);
+//     process.exit(1);
+// }).finally(async ()=>{
+//     await Prisma.$disconnect();
+// });
+deleteGift('65f1bacc3037064e96f5bbcf').catch((err) => {
     console.error('Error: ', err);
     process.exit(1);
 }).finally(async ()=>{
